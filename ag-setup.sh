0e200a69e634e5819c02f290eaec007cb60e63fb
ka <ka.kaosf@gmail.com>
2015-07-21 17:16:06 +0900

    Fix ag-setup.sh

--- ag-setup.sh
+++ ag-setup.sh
@@ -1,5 +1,7 @@
 # ref. https://github.com/ggreer/the_silver_searcher
+git clone https://github.com/ggreer/the_silver_searcher
+cd the_silver_searcher
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install pcre-devel xz-devel
-./build
+./build.sh
 sudo make install
