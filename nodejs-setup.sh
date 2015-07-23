8aa40b4d7fcef757c5824527a49ac60992aa03cb
ka <ka.kaosf@gmail.com>
2015-07-23 17:56:51 +0900

    Create nodejs-setup.sh

---
+++ nodejs-setup.sh
@@ -0,0 +1,11 @@
+# install Node.js with nodebrew
+# ref. https://github.com/hokaccha/nodebrew
+
+curl -L git.io/nodebrew | perl - setup
+echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> $HOME/.zshenv
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+
+# nodebrew help
+# nodebrew ls-all
+nodebrew install-binary 0.12
+nodebrew use latest
