83553897a31cb81bcc26aacd8fbd855a2ae4d1d2
ka <ka.kaosf@gmail.com>
2015-09-27 21:10:05 +0900

    Update go-setup.sh

--- go-setup.sh
+++ go-setup.sh
@@ -6,7 +6,14 @@ echo 'export GOROOT=$HOME/.go' >> $HOME/.zshenv
 echo 'export PATH=$PATH:$GOROOT/bin' >> $HOME/.zshenv
 echo 'export GOPATH=$HOME/.gocode' >> $HOME/.zshenv
 echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
-source $HOME/.zshenv
+echo 'export GOROOT=$HOME/.go' >> $HOME/.profile
+echo 'export PATH=$PATH:$GOROOT/bin' >> $HOME/.profile
+echo 'export GOPATH=$HOME/.gocode' >> $HOME/.profile
+echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.profile
+export GOROOT=$HOME/.go
+export PATH=$PATH:$GOROOT/bin
+export GOPATH=$HOME/.gocode
+export PATH=$HOME/.gocode/bin:$PATH
 rm -f go${VERSION}.linux-amd64.tar.gz
 
 go get github.com/peco/peco/cmd/peco
