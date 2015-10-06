d97fb2ccd885562a924ee6bf92065d8c869cf74c
ka <ka.kaosf@gmail.com>
2015-10-06 23:35:08 +0900

    Install Git globally

--- git-setup.sh
+++ git-setup.sh
@@ -1,10 +1,12 @@
-sudo yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils
-mkdir -p ~/local/src
-cd ~/local/src
-wget https://www.kernel.org/pub/software/scm/git/git-2.3.7.tar.gz
-tar xzvf git-2.3.7.tar.gz
+sudo yum -y install \
+  curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils
+curl -O https://www.kernel.org/pub/software/scm/git/git-2.3.7.tar.gz
+tar xf git-2.3.7.tar.gz
 cd git-2.3.7
-make prefix=$HOME/local all
-make prefix=$HOME/local install
+make prefix=/usr/local all
+sudo make prefix=/usr/local install
+cd -
+sudo mv git-2.3.7 /usr/local/src
+rm git-2.3.7.tar.gz
 
 # ref. http://blog.clock-up.jp/entry/2015/05/01/centos-git-build
