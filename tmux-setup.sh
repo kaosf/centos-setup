b42db8c07f7612d937f62d4bbdbab67b94fba482
ka <ka.kaosf@gmail.com>
2015-10-08 01:21:40 +0900

    Install libevent and tmux globally

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,6 +1,4 @@
 sudo yum remove -y libevent libevent-devel libevent-headers
-mkdir -p ~/local/src
-cd ~/local/src
 wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
 tar xf libevent-2.0.21-stable.tar.gz
 cd libevent-2.0.21-stable
@@ -8,15 +6,18 @@ cd libevent-2.0.21-stable
 make
 sudo make install
 sudo ln -s /usr/local/lib/libevent-2.0.so.5 /usr/lib64/libevent-2.0.so.5
+cd -
+sudo mv libevent-2.0.21-stable /usr/local/src
+rm libevent-2.0.21-stable.tar.gz
 
-sudo yum -y install ncurses-devel
-mkdir ~/local/src
-cd ~/local/src
 wget http://downloads.sourceforge.net/tmux/tmux-2.0.tar.gz
-tar xzf tmux-2.0.tar.gz
+tar xf tmux-2.0.tar.gz
 cd tmux-2.0
-./configure --prefix=$HOME/local
+./configure
 make
-make install
+sudo make install
+cd -
+sudo mv tmux-2.0 /usr/local/src
+rm tmux-2.0.tar.gz
 
 # ref. https://jtskarbek.wordpress.com/2013/03/22/tmux-compile-failure/
