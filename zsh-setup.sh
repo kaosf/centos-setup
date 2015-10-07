a9ff7b808850e29b9527144ea238818c8b267ed4
ka <ka.kaosf@gmail.com>
2015-10-07 23:00:01 +0900

    Install zsh globally

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,12 +1,12 @@
-mkdir -p ~/local/src
-cd ~/local/src
-git clone git://git.code.sf.net/p/zsh/code zsh
+sudo yum -y install ncurses-devel
+git clone https://github.com/zsh-users/zsh
 cd zsh
 git checkout zsh-5.0.8
 ./Util/preconfig
-./configure --prefix=$HOME/local --enable-multibyte
+./configure --prefix=/usr/local --enable-multibyte
 make clean
 make
-sudo make install
+sudo make install; [ $? = 2 ]
+cd -
+sudo mv zsh /usr/local/src
 echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/zsh
