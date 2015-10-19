7ccf24808ab81eaeca0f6218717f4a117d60de27
ka <ka.kaosf@gmail.com>
2015-10-19 15:35:40 +0900

    Fix unsecure-root-setup.sh a little

--- unsecure-root-setup.sh
+++ unsecure-root-setup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 if [ $# -lt 1 ]; then exit 1; fi
 NAME=$1
 groupadd $NAME
@@ -6,7 +5,7 @@ useradd $NAME -g $NAME -s /bin/bash -d /home/$NAME
 echo "${NAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 mkdir /home/${NAME}/.ssh
 cp .ssh/authorized_keys /home/${NAME}/.ssh/
-chown ka:ka -R /home/${NAME}/.ssh
+chown ${NAME}:${NAME} -R /home/${NAME}/.ssh
 chmod 600 /home/${NAME}/.ssh/authorized_keys
-sudo sed 's/PermitRootLogin yes/PermitRootLogin no/' -i /etc/ssh/sshd_config
-sudo service sshd restart
+sed 's/PermitRootLogin yes/PermitRootLogin no/' -i /etc/ssh/sshd_config
+service sshd restart
