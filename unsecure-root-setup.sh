ab1ff0e95d03545332f8cbcc2aca2ae9425aa3f4
ka <ka.kaosf@gmail.com>
2015-11-22 22:05:41 +0900

    Fix unsecure-root-setup.sh

--- unsecure-root-setup.sh
+++ unsecure-root-setup.sh
@@ -7,5 +7,5 @@ mkdir /home/${NAME}/.ssh
 cp .ssh/authorized_keys /home/${NAME}/.ssh/
 chown ${NAME}:${NAME} -R /home/${NAME}/.ssh
 chmod 600 /home/${NAME}/.ssh/authorized_keys
-sed 's/PermitRootLogin yes/PermitRootLogin no/' -i /etc/ssh/sshd_config
+sed 's/#PermitRootLogin yes/PermitRootLogin no/' -i /etc/ssh/sshd_config
 service sshd restart
